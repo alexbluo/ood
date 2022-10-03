aa5144dbc2e0a5ff21289533035da31d02db9ed6
Alex Luo <alexbluo01@gmail.com>
2022-10-03 12:02:20 -0400

    switch to nextjs

---
+++ gtq/frontend/.eslintrc.js
@@ -0,0 +1,63 @@
+module.exports = {
+  extends: [
+    "airbnb",
+    "airbnb-typescript",
+    "next/core-web-vitals",
+    "plugin:import/recommended",
+    "plugin:import/typescript",
+    "prettier",
+  ],
+  ignorePatterns: "*.js",
+  parserOptions: {
+    project: "tsconfig.json",
+    tsconfigRootDir: __dirname,
+  },
+  rules: {
+    quotes: "off",
+    camelcase: "off",
+    "no-console": "off",
+    "import/no-extraneous-dependencies": "off",
+    "no-param-reassign": "off",
+    "arrow-body-style": "off",
+    "react/prop-types": "off",
+    "react/jsx-props-no-spreading": "off",
+    "react/react-in-jsx-scope": "off",
+    "react/self-closing-comp": "off",
+    "react/button-has-type": "off",
+    "react/function-component-definition": [
+      "error",
+      {
+        namedComponents: "arrow-function",
+        unnamedComponents: "arrow-function",
+      },
+    ],
+    "no-restricted-syntax": "off",
+    "import/order": [
+      "error",
+      {
+        groups: ["builtin", "external", "internal"],
+        pathGroups: [
+          {
+            pattern: "react",
+            group: "external",
+            position: "before",
+          },
+        ],
+        pathGroupsExcludedImportTypes: ["react"],
+        "newlines-between": "never",
+        alphabetize: {
+          order: "asc",
+          caseInsensitive: true,
+        },
+      },
+    ],
+    "jsx-a11y/anchor-is-valid": [
+      "error",
+      {
+        components: ["Link"],
+        specialLink: ["hrefLeft", "hrefRight"],
+        aspects: ["invalidHref", "preferButton"],
+      },
+    ],
+  },
+};
---
+++ gtq/frontend/pages/_app.tsx
@@ -0,0 +1,8 @@
+import "../index.css";
+import { AppProps } from "next/app";
+
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  return <Component {...pageProps} />;
+};
+
+export default MyApp;
