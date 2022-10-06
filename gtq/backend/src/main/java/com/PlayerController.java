47a2417677fbe0f7f289bd901ca48b8b68765029
Alex Luo <alexbluo01@gmail.com>
2022-10-06 13:24:28 -0400

    connect to postgres and proxy nextjs

--- gtq/backend/src/main/java/com/PlayerController.java
+++ gtq/backend/src/main/java/com/PlayerController.java
@@ -1,13 +1,37 @@
 package com;
 
+import java.sql.*;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PlayerController {
+  private static final Connection CON = connect();
+
+  /**
+   * Establishes a connection to the server with the specified URL, USERNAME, and
+   * PASSWORD.
+   * 
+   * @return a connection to the server with the specified URL, USERNAME, and
+   *         PASSWORD.
+   */
+  private static Connection connect() {
+    final String URL = "jdbc:postgresql://localhost:5432/gtq";
+    final String USERNAME = "alexbluo";
+    final String PASSWORD = "";
+
+    Connection c = null;
+    try {
+      c = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+    } catch (SQLException ex) {
+      ex.printStackTrace();
+    }
+    return c;
+  }
 
   @GetMapping("/")
   public Player index() {
+
     return new Player(1, 1, "BUHMBO", 1);
   }
 
