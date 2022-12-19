a5c9636b2aa1892fee2ba6be76f6592e7fed4e1c
Alex Luo <alexbluo01@gmail.com>
2022-12-19 12:08:36 -0500

    add dalle request

--- wheres-dalle/pages/index.tsx
+++ wheres-dalle/pages/index.tsx
@@ -1,59 +1,76 @@
 import { useState } from "react";
 import axios from "axios";
-import { useQuery } from "react-query";
+import { useMutation, useQuery } from "react-query";
 import Tile from "../components/Tile";
 
 const Index = () => {
+  const { round, setRound } = useState(0);
+
   // const {
   //   data: unsplash,
-  //   error,
-  //   isLoading,
-  //   isError,
+  //   isLoading: unsplashIsLoading,
+  //   isError: unsplashIsError,
   // } = useQuery(["unsplash"], async () => {
   //   const res = await axios.get(
-  //     `https://api.unsplash.com/photos/random?client_id=${process.env.NEXT_PUBLIC_UNSPLASH_ACCESS_KEY}&count=23&orientation=squarish`
+  //     `https://api.unsplash.com/photos/random?client_id=${process.env.NEXT_PUBLIC_UNSPLASH_ACCESS_KEY}&count=10&orientation=squarish`
   //   );
 
-  //   return res.data.map((image: any) => {
-  //     return image.urls.full;
-  //   });
+  //   console.log(res);
+  //   return {
+  //     url: res.data[round].urls.full,
+  //     description: res.data[round].alt_description,
+  //   };
   // });
+  const unsplash = {
+    url: "https://images.unsplash.com/photo-1670969490504-0f1be0abfc90?crop=entropy&cs=tinysrgb&fm=jpg&ixid=MnwzODkyNDZ8MHwxfHJhbmRvbXx8fHx8fHx8fDE2NzExMjU0NjM&ixlib=rb-4.0.3&q=80",
+    description:
+      "a christmas card with a picture of a christmas tree and a white box",
+  };
 
-  // TODO: could do one or the other image variation
-  const unsplash = [
-    "https://images.unsplash.com/photo-1670969490504-0f1be0abfc90?crop=entropy&cs=tinysrgb&fm=jpg&ixid=MnwzODkyNDZ8MHwxfHJhbmRvbXx8fHx8fHx8fDE2NzExMjU0NjM&ixlib=rb-4.0.3&q=80",
-    "https://images.unsplash.com/photo-1668449003991-42e6e9e635ec?crop=entropy&cs=tinysrgb&fm=jpg&ixid=MnwzODkyNDZ8MHwxfHJhbmRvbXx8fHx8fHx8fDE2NzExMjU0NjM&ixlib=rb-4.0.3&q=80",
-    "https://images.unsplash.com/photo-1669406780538-e57f8c0b248e?crop=entropy&cs=tinysrgb&fm=jpg&ixid=MnwzODkyNDZ8MHwxfHJhbmRvbXx8fHx8fHx8fDE2NzExMjU0NjM&ixlib=rb-4.0.3&q=80",
-    "https://images.unsplash.com/photo-1670705848498-0253286bae3a?crop=entropy&cs=tinysrgb&fm=jpg&ixid=MnwzODkyNDZ8MHwxfHJhbmRvbXx8fHx8fHx8fDE2NzExMjU0NjM&ixlib=rb-4.0.3&q=80",
-    "https://images.unsplash.com/photo-1667833183058-c13008862b87?crop=entropy&cs=tinysrgb&fm=jpg&ixid=MnwzODkyNDZ8MHwxfHJhbmRvbXx8fHx8fHx8fDE2NzExMjU0NjM&ixlib=rb-4.0.3&q=80",
-    "https://images.unsplash.com/photo-1669720020306-cb84c67b5e97?crop=entropy&cs=tinysrgb&fm=jpg&ixid=MnwzODkyNDZ8MHwxfHJhbmRvbXx8fHx8fHx8fDE2NzExMjU0NjM&ixlib=rb-4.0.3&q=80",
-    "https://images.unsplash.com/photo-1669484179894-4cfb13b51b7d?crop=entropy&cs=tinysrgb&fm=jpg&ixid=MnwzODkyNDZ8MHwxfHJhbmRvbXx8fHx8fHx8fDE2NzExMjU0NjM&ixlib=rb-4.0.3&q=80",
-    "https://images.unsplash.com/photo-1669650853522-fe5a4b89a119?crop=entropy&cs=tinysrgb&fm=jpg&ixid=MnwzODkyNDZ8MHwxfHJhbmRvbXx8fHx8fHx8fDE2NzExMjU0NjM&ixlib=rb-4.0.3&q=80",
-    "https://images.unsplash.com/photo-1669501084883-85849ad2d40f?crop=entropy&cs=tinysrgb&fm=jpg&ixid=MnwzODkyNDZ8MHwxfHJhbmRvbXx8fHx8fHx8fDE2NzExMjU0NjM&ixlib=rb-4.0.3&q=80",
-    "https://images.unsplash.com/photo-1669725337418-f81459093abd?crop=entropy&cs=tinysrgb&fm=jpg&ixid=MnwzODkyNDZ8MHwxfHJhbmRvbXx8fHx8fHx8fDE2NzExMjU0NjM&ixlib=rb-4.0.3&q=80",
-    "https://images.unsplash.com/photo-1667736697093-b42d29d0dc73?crop=entropy&cs=tinysrgb&fm=jpg&ixid=MnwzODkyNDZ8MHwxfHJhbmRvbXx8fHx8fHx8fDE2NzExMjU0NjM&ixlib=rb-4.0.3&q=80",
-    "https://images.unsplash.com/photo-1670481382179-3968ad563542?crop=entropy&cs=tinysrgb&fm=jpg&ixid=MnwzODkyNDZ8MHwxfHJhbmRvbXx8fHx8fHx8fDE2NzExMjU0NjM&ixlib=rb-4.0.3&q=80",
-    "https://images.unsplash.com/photo-1669390412953-677ec75ae6d0?crop=entropy&cs=tinysrgb&fm=jpg&ixid=MnwzODkyNDZ8MHwxfHJhbmRvbXx8fHx8fHx8fDE2NzExMjU0NjM&ixlib=rb-4.0.3&q=80",
-    "https://images.unsplash.com/photo-1668202190149-43b18156d5f4?crop=entropy&cs=tinysrgb&fm=jpg&ixid=MnwzODkyNDZ8MHwxfHJhbmRvbXx8fHx8fHx8fDE2NzExMjU0NjM&ixlib=rb-4.0.3&q=80",
-    "https://images.unsplash.com/photo-1669050589643-a06e01d7391e?crop=entropy&cs=tinysrgb&fm=jpg&ixid=MnwzODkyNDZ8MHwxfHJhbmRvbXx8fHx8fHx8fDE2NzExMjU0NjM&ixlib=rb-4.0.3&q=80",
-    "https://images.unsplash.com/photo-1670944662141-d3753516873c?crop=entropy&cs=tinysrgb&fm=jpg&ixid=MnwzODkyNDZ8MHwxfHJhbmRvbXx8fHx8fHx8fDE2NzExMjU0NjM&ixlib=rb-4.0.3&q=80",
-    "https://images.unsplash.com/photo-1669846510648-95ffffb8e269?crop=entropy&cs=tinysrgb&fm=jpg&ixid=MnwzODkyNDZ8MHwxfHJhbmRvbXx8fHx8fHx8fDE2NzExMjU0NjM&ixlib=rb-4.0.3&q=80",
-    "https://images.unsplash.com/photo-1669307260367-8d5e7d61b9b9?crop=entropy&cs=tinysrgb&fm=jpg&ixid=MnwzODkyNDZ8MHwxfHJhbmRvbXx8fHx8fHx8fDE2NzExMjU0NjM&ixlib=rb-4.0.3&q=80",
-    "https://images.unsplash.com/photo-1670991761566-534b07539db5?crop=entropy&cs=tinysrgb&fm=jpg&ixid=MnwzODkyNDZ8MHwxfHJhbmRvbXx8fHx8fHx8fDE2NzExMjU0NjM&ixlib=rb-4.0.3&q=80",
-    "https://images.unsplash.com/photo-1669721166543-de1035adc9cf?crop=entropy&cs=tinysrgb&fm=jpg&ixid=MnwzODkyNDZ8MHwxfHJhbmRvbXx8fHx8fHx8fDE2NzExMjU0NjM&ixlib=rb-4.0.3&q=80",
-    "https://images.unsplash.com/photo-1670362246724-ce120df90392?crop=entropy&cs=tinysrgb&fm=jpg&ixid=MnwzODkyNDZ8MHwxfHJhbmRvbXx8fHx8fHx8fDE2NzExMjU0NjM&ixlib=rb-4.0.3&q=80",
-    "https://images.unsplash.com/photo-1670845494093-0bd5704e1e0d?crop=entropy&cs=tinysrgb&fm=jpg&ixid=MnwzODkyNDZ8MHwxfHJhbmRvbXx8fHx8fHx8fDE2NzExMjU0NjM&ixlib=rb-4.0.3&q=80",
-    "https://images.unsplash.com/photo-1670348282804-a66da753b87e?crop=entropy&cs=tinysrgb&fm=jpg&ixid=MnwzODkyNDZ8MHwxfHJhbmRvbXx8fHx8fHx8fDE2NzExMjU0NjM&ixlib=rb-4.0.3&q=80",
-  ];
+  // TODO: wait for unsplash
+  const {
+    data: dalle,
+    isLoading: dalleIsLoading,
+    isError: dalleIsError,
+  } = useQuery(
+    ["dalle"],
+    async () => {
+      const res = await axios.post(
+        "https://api.openai.com/v1/images/generations",
+        { prompt: unsplash!.description, n: 1, size: "1024x1024" },
+        {
+          headers: {
+            Authorization: `Bearer ${process.env.NEXT_PUBLIC_DALLE_KEY}`,
+          },
+        }
+      );
 
-  // choose random number 1-24, if equals random in map then dalle
-  // if (isError) return <div>error</div>
+      return res.data[0].url;
+    }
+    // { enabled: !unsplashIsLoading }
+  );
+
+  // if (unsplashIsError) return <div>error</div>;
+  if (dalleIsError) return <div>error</div>;
   return (
     <div className="p-16">
-      <div className="grid grid-cols-6 grid-rows-4 w-full border border-black">
-        {/*! isLoading && */ unsplash.map((image) => (
-          <Tile src={image} alt={image} type="unsplash" key={image} />
-        ))}
+      <div className="grid grid-cols-2 w-full border border-black">
+        {!dalleIsLoading && (
+          <>
+            <Tile
+              src={unsplash!.url}
+              alt={unsplash!.description}
+              type="unsplash"
+              key={unsplash!.url}
+            />
+            <Tile
+              src={dalle.url}
+              alt={unsplash.description}
+              type="dalle"
+              key={dalle.url}
+            />
+          </>
+        )}
       </div>
     </div>
   );
